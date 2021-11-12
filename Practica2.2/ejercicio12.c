bf8da267e61f88499de951133dd29bd57bc286fb
Gorka Silva <89659071+GorkaOlimpico@users.noreply.github.com>
2021-11-12 02:26:57 +0100

    Create ejercicio12.c

---
+++ Practica2.2/ejercicio12.c
@@ -0,0 +1,20 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc, char **argv){
+
+   int fd = open(argv[1], O_CREAT | O_RDWR, 0777);
+   if (fd == -1){
+      printf("Error al abrir el archivo\n");
+      return -1;
+   }
+
+   dup2(fd,1);
+
+   printf("Se ha redirigido la salida del fichero\n", argv[1]);
+
+   return 0;
+}
