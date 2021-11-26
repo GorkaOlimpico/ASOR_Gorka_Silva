bdfb1fcd6fbb83793dfe62fea1964d43b9eece9b
Gorka Silva <89659071+GorkaOlimpico@users.noreply.github.com>
2021-11-26 08:41:43 +0100

    Update ejercicio7_2.c

--- Practica2.3/ejercicio7_2.c
+++ Practica2.3/ejercicio7_2.c
@@ -2,15 +2,27 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
-
+#include <string.h>
 
 int main(int argc, char **argv) {
 
-   if(system(argv[1]) == -1){
-      perror("Error\n");
+   int cmdLen = 1;
+   int i;
+   for (i = 1; i < argc; i++){
+      cmdLen += strlen(argv[i]) + 1;
+   }
+   char *cmd = malloc(sizeof(char)*cmdLen);
+   strcpy(cmd, "");
+
+   for (i = 1; i < argc; i++) {
+      strcat(cmd, argv[i]);
+      strcat(cmd, " ");
+  }
+
+   if(system(cmd) == -1){
+      perror("Error en system\n");
    }
 
    printf("El comando termino de ejecutarse\n");
    return 0;
 }
-
