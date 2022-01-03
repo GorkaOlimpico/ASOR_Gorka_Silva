5f5e803db12308ca089d61db5a3d956417bf5505
Gorka Silva <89659071+GorkaOlimpico@users.noreply.github.com>
2022-01-03 15:01:47 +0100

    Update ejercicio1.c

--- Practica2.5/ejercicio1.c
+++ Practica2.5/ejercicio1.c
@@ -19,16 +19,10 @@ int main(int argc, char **argv) {
    struct addrinfo hints;
    struct addrinfo *resultado, *iterator;
 
-   memset(&hints, 0, sizeof(struct addrinfo));
-
    hints.ai_flags = AI_PASSIVE;
    hints.ai_family = AF_UNSPEC;
    hints.ai_socktype = 0;
-   hints.ai_protocol = 0;
-   hints.ai_addrlen = NULL;
-   hints.ai_addr = NULL;
-   hints.ai_canonname = NULL;
-   hints.ai_next = NULL;
+
 
    if(getaddrinfo(argv[1], NULL, &hints, &resultado) != 0){
       printf("Error al ejecutar addrinfo\n");
@@ -40,13 +34,6 @@ int main(int argc, char **argv) {
          case AF_INET:;
             struct sockaddr_in *info = iterator->ai_addr;
             char ip[INET_ADDRSTRLEN + 1]="";
-            inet_ntop(AF_INET, &(info->sin_addr), ip, INET_ADDRSTRLEN + 1);
-            printf("%s\t", ip);
-         break;
-
-         case AF_INET6:;
-            struct sockaddr_in6 *info6 = iterator->ai_addr;
-            char ipv6[INET6_ADDRSTRLEN + 1]="";
             inet_ntop(AF_INET, &(info->sin_addr), ip, INET_ADDRSTRLEN + 1);
             printf("%s\t", ip);
          break;
